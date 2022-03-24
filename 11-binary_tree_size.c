147dfb342359f20f122810c34c4f0ef5e577d1c2
Andrés Cataño <3866@holbertonschool.com>
2022-03-24 11:05:38 -0500

    First Commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,13 +12,13 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size_left = 0;
-    size_t size_right = 0;
+	size_t size_right = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	size_left = binary_tree_size(tree->left);
-    size_right = binary_tree_size(tree->right);
+	size_right = binary_tree_size(tree->right);
 
 	return (size_left + size_right + 1);
 

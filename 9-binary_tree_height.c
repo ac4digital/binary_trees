50501915463c1ed87b574831ebe805d0e37196f6
Andrés Cataño <3866@holbertonschool.com>
2022-03-24 20:36:03 -0500

    First Commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
